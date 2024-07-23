67997fbf8791d916c57b842a463622a98b5165d3
Sam Lam <xlin67948gmail.com>
2024-07-23 15:20:23 +0800

    Fundamental part is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import {Routes, Route, Link} from 'react-router-dom';
+import Fundamentals from './components/Fundamentals';
 import './App.scss';
 
 const App = () => {
   return (
     <div>
-      <h1>Start</h1>
+      <h1>FM-Animation</h1>
+      <nav>
+        <Link to='/fundamental'>Fundamental</Link>
+      </nav>
+      <Routes>
+        <Route path='/fundamental' element={<Fundamentals />}/>
+      </Routes>
     </div>
   )
 }
