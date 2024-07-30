5806ae9da9a0e0416e0f591ecc189371045a2ff6
Sam Lam <xlin67948gmail.com>
2024-07-30 13:34:37 +0800

    this part have some problem

--- src/components/Choreography-04.tsx
+++ src/components/Choreography-04.tsx
@@ -1,12 +1,12 @@
+import { ReactNode } from "react";
 
 const Choreography = () => {
+  const balls = [1, 2, 3];
   return (
     <div className="app">
         <h1>Choreography</h1>
         <div className="container">
-            <div className="container--ball"></div>
-            <div className="container--ball"></div>
-            <div className="container--ball"></div>
+            {balls.map((ball, i) => <div className="container--ball" key={i} style={{'--i': i}}></div>)}
         </div>
     </div>
   )
